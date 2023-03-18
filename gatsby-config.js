0ccc8362a900510c9347f31a63367a9a96b7db5a
Robert <robert.sergio.eng@gmail.com>
2023-03-18 18:19:20 -0300

    recreated project

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,31 +6,5 @@ module.exports = {
     title: `gatsby_template`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [
-    {
-      resolve: `gatsby-plugin-s3`,
-      options: {
-        bucketName: "gatsbystatic",
-      },
-    },
-    "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+  plugins: ["gatsby-plugin-sitemap"]
 };
