e3f5a0812acf2b3277e58dad2e1389538df985a9
Robert <robert.sergio.eng@gmail.com>
2023-03-25 16:03:00 -0300

    remove sidebar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Footer from './Footer'
 import Navbar from './Navbar'
-import Sidebar from './Sidebar'
 
 export default function layout() {
   return (
     <section>
         <Navbar />
-        <Sidebar />
         <Footer />
     </section>
   )
