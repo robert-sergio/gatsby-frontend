493b8bdb307d845aa62c3c30cec2118b43b1f039
Robert <robert.sergio.eng@gmail.com>
2023-03-25 15:15:59 -0300

    test types

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout"
 import '../styles/styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-require('bootstrap/dist/js/bootstrap.min.js')
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 const IndexPage = () => {
   return (
