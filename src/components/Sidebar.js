566762a600f14366cce21cebc2cb800299e501a8
Robert <robert.sergio.eng@gmail.com>
2023-03-25 14:17:12 -0300

    test import bootstrap with relative path

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 export default function sidebar() {
   return (
-    <div>
-      <div>
-        <p>Sidebar</p>
+    <div className="offcanvas offcanvas-start show" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel">
+      <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasLabel">Offcanvas</h5>
+          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div className="offcanvas-body">
+          Content for the offcanvas goes here. You can place just about any Bootstrap component or custom elements here.
       </div>
     </div>
   )
