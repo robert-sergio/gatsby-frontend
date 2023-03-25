7e09b66ca4ca22a712b0ecb06f00979d7300521e
Robert <robert.sergio.eng@gmail.com>
2023-03-25 16:44:11 -0300

    using reactstrap

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Footer from './Footer'
-import Navbar from './Navbar'
+import MeuNavbar from './Navbar'
 
 export default function layout() {
   return (
     <section>
-        <Navbar />
-        <Footer />
+        <MeuNavbar />
     </section>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,33 +7,65 @@ import {
   Nav,
   NavItem,
   NavLink,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
 } from 'reactstrap';
 
-export default function Navbar(props) {
+export default function MeuNavbar({ direction, ...args }) {
   const [collapsed, setCollapsed] = useState(true);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [dropdownOpen2, setDropdownOpen2] = useState(false);
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+  const toggle2 = () => setDropdownOpen2((prevState) => !prevState);
 
   return (
     <div>
-      <Navbar color="faded" light>
-        <NavbarBrand href="/" className="me-auto">
-          reactstrap
-        </NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className="me-2" />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+        <Navbar color="faded" light className=' bg-secondary px-5'>
+            <NavbarBrand href="/" className="text-light fs-4 myfont">
+                Robert Sergio
+            </NavbarBrand>
+            <Nav navbar-expand className='position-relative'>
+                <NavItem >
+                    <Dropdown isOpen={dropdownOpen} toggle={toggle} direction={direction}>
+                        <DropdownToggle caret>Aulas</DropdownToggle>
+                        <DropdownMenu {...args}>
+                        <DropdownItem>Python</DropdownItem>
+                        <DropdownItem>SQL</DropdownItem>
+                        <DropdownItem>Django</DropdownItem>
+                        <DropdownItem>Selenium</DropdownItem>
+                        <DropdownItem>Arduino</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </NavItem>
+                <NavItem >
+                    <Dropdown isOpen={dropdownOpen2} toggle={toggle2} direction={direction}>
+                        <DropdownToggle caret>Projetos</DropdownToggle>
+                        <DropdownMenu {...args}>
+                        <DropdownItem>Incubadora de Feijões</DropdownItem>
+                        <DropdownItem>Central meteorologica</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </NavItem>
+                <NavItem>
+                    <NavLink className='text-light' href="/components/">Sobre</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className='text-light' href="/components/">Contatos</NavLink>
+                </NavItem>
+                <NavItem>
+                    <i class="bi bi-github"></i>
+                </NavItem>
+                <NavItem>
+                    <i class="bi bi-github"></i>
+                </NavItem>
+                <NavItem>
+                    <i class="bi bi-github"></i>
+                </NavItem>
+            </Nav>
+        </Navbar>
     </div>
   );
 }
